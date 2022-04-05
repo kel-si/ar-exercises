431d3543fc293223a13561568359c8077d871ac5
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-05 17:41:10 -0600

    add label for printing

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,6 +8,6 @@ store = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true
 store = Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
 store = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts Store.count
+puts "Store.count:", Store.count
 
 
