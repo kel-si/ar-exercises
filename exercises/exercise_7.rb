ee85a10fc61305ce6d131a6593377abd4689ebf9
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-06 12:46:47 -0600

    display error message

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -31,3 +31,12 @@ puts "----------"
 # puts store_test = Store.create(name: 'Test', annual_revenue: -20 , mens_apparel: true, womens_apparel: true).valid? # --> false
 
 puts store_test = Store.create(name: 'Test', annual_revenue: 5 , mens_apparel: true, womens_apparel: true).valid? # --> true
+
+puts "What is the store name?"
+store_name = gets.chomp
+
+puts "Got it! Store name is #{store_name}. Let's see if that is a valid input..."
+
+store = Store.create(name: "#{store_name}", annual_revenue: nil , mens_apparel: nil, womens_apparel: nil)
+
+puts store.errors.full_messages
