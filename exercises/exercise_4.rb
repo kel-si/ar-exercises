d2e2c7fdbf4933636077112704920efb16711589
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-05 21:51:55 -0600

    women's stores with < one million in annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -14,6 +14,11 @@ store = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: tru
 @mens_stores = Store.where(mens_apparel: true) 
 
 @mens_stores.each do | store |
-  puts "#{store.name}: #{store.annual_revenue}"
+  puts "Men's Stores: #{store.name}: #{store.annual_revenue}"
 end
 
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000") 
+
+@womens_stores.each do | store |
+puts "Women's Stores: #{store.name}: #{store.annual_revenue}"
+end
