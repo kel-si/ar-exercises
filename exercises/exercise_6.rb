718bdb0572604c8f67c350a307a2ca0bdb52b13d
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-06 10:57:28 -0600

    add employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Jo", last_name: "Sephine", hourly_rate: 100)
+@store1.employees.create(first_name: "Mont", last_name: "Gomery", hourly_rate: 85)
+
+
+@store2.employees.create(first_name: "Frosty", last_name: "Snowman", hourly_rate: 80)
+@store2.employees.create(first_name: "Daisy", last_name: "Doodle", hourly_rate: 100)
